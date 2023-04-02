81017031c45192061e538896ff8406158c48e3d5
Borna Bešić <git.o8yim@aleeas.com>
2023-04-02 15:32:04 +0200

    Add kex::read_file() utility

---
+++ src/kex/utils.cpp
@@ -0,0 +1,37 @@
+/*
+Kex: Plug-and-play 2D graphics C++ library built on top of OpenGL ES 3.0 API
+Copyright (C) 2023  Borna Bešić
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <kex/utils.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+
+namespace kex {
+
+    std::vector<char> read_file(const std::string &path) {
+        std::ifstream input_stream(path);
+        input_stream.seekg(0, std::ios::end);
+        const auto file_size = input_stream.tellg();
+        input_stream.seekg(0, std::ios::beg);
+
+        std::vector<char> file_bytes(file_size);
+        input_stream.read(file_bytes.data(), file_size);
+        return file_bytes;
+    }
+
+}
